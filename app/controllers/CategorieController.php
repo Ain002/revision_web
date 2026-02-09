a7f5a304a7995360f191f5f5ae9327fcdb669f81
randianina07 <randianina07@gmail.com>
2026-02-09 16:35:05 +0300

    controller categorie

---
+++ app/controllers/CategorieController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\CategorieModel;
+use flight\Engine;
+use Flight;
+
+class CategorieController{
+    protected Engine $app;
+
+	public function __construct($app) {
+		$this->app = $app;
+	}
+
+    public function getAllCategories(){
+        $db = Flight::db();
+        $model = new CategorieModel($db);
+        $categories = $model->getAllCategories();
+        $this->app->render('categories', ['categories' => $categories]);
+    }
+
+    public function insertCategorie(){
+        $db = Flight::db();
+
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            Flight::redirect('/');
+            return;
+        }
+        $nom   = $_POST['nom'];
+
+        $model = new CategorieModel($db);
+        $model->insertCategorie($nom);
+
+        Flight::redirect('/listCategories');
+    }
+
+    public function removeCategorie($id) {
+        $db = Flight::db();
+        $model = new CategorieModel($db);
+        $model->removeCategorie($id);
+        Flight::redirect('/listCategories');
+    }
+
+    public function updateCategorie(){
+        $db = Flight::db();
+
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            Flight::redirect('/');
+            return;
+        }
+        $nom   = $_POST['nom'];
+
+        $model = new CategorieModel($db);
+        $model->updateCategorie($nom);
+
+        Flight::redirect('/listCategories');
+    }
+
+}
+;
